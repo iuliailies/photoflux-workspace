3da46c85025e7f3948a6303bf68060c230650693
Iulia Ilies <iulia.ilies01@gmail.com>
2025-06-30 11:25:58 +0300

    presentation setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,10 @@
 export const environment = {
   production: false,
   // apiURL: 'http://localhost:8033/api',
-  apiURL: 'http://backendpf.services.ozoniuss.com/api',
+  // apiURL: 'http://backendpf.services.ozoniuss.com/api',
+  // apiURL: 'http://192.168.0.100:8033/api',
+  apiURL: 'http://ozoniuss.go.ro:8033/api',
+
 };
 
 /*
